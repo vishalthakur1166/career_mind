1623fe8c99691e5f58fe9401d9712ec0737e7534
vishalthakur <vishalthakur1160@gmail.com>
2025-07-06 17:14:45 -0500

    Changes via Vishal

---
+++ backend/career_mind/src/main/java/com/backend/career_mind/CareerMindApplication.java
@@ -0,0 +1,13 @@
+package com.backend.career_mind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerMindApplication.class, args);
+	}
+
+}
